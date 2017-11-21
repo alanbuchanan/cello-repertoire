0ffe14f60584bb03133e557c81f6aaa7f2927bbf
Rory Smith <rory.smith@korelogic.co.uk>
2017-11-21 18:47:33 +0000

    Remove chinese messages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { LocaleProvider } from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LocaleProvider locale={enUS}><App /></LocaleProvider>, document.getElementById('root'));
 registerServiceWorker();

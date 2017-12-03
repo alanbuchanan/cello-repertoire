0fb74ff25e0aadaa1efc87cfe1b808bdc5f7ff49
Rory Smith <rory.smith@korelogic.co.uk>
2017-12-03 14:46:33 +0000

    Add mobile styles and components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import _ from 'lodash';
 import 'antd/dist/antd.css';
 import { Table, Input, Button, Icon, Select, Slider, Tooltip, Form } from 'antd';
-import { Accordion, List, Popover } from 'antd-mobile';
+import { Accordion, List } from 'antd-mobile';
 import 'antd-mobile/dist/antd-mobile.css';
 const FormItem = Form.Item;
 const Item = List.Item;
@@ -27,7 +27,6 @@ class App extends Component {
   };
 
   onSelect = (opt) => {
-    // console.log(opt.props.value);
     this.setState({
       visible: false,
       selected: opt.props.value,
@@ -192,6 +191,10 @@ class App extends Component {
     return 0;
   }
 
+  isDeviceMobile() {
+    return /Android|BB10|PlayBook|webOS|iPhone|iPod|Opera Mini|IEMobile/i.test(navigator.userAgent)
+  }
+
   render() {
     const linkLayout = {
       style: {
@@ -202,6 +205,13 @@ class App extends Component {
     const iconLayout = {
       style: {
         fontSize: '16px',
+      }
+    }
+
+    const iconLayoutMobile = {
+      style: {
+        fontSize: '20px',
+        marginRight: '10px',
       }
     }
 
@@ -314,6 +324,14 @@ class App extends Component {
       }
     }
 
+    const inputWithRemoveIconLayoutMobile = {
+      style: {
+        width: '90%',
+        marginRight: '8px',
+        fontSize: '20px',
+      }
+    }
+
     const deleteIconProps = {
       style: {
         cursor: 'pointer',
@@ -322,12 +340,12 @@ class App extends Component {
       type: "minus-circle-o",
     }
 
-    if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
-      return <div>
-
+    const formItems = (layout) => {
+      return ([
         <FormItem
           {...formItemLayout}
           label="Composer"
+          key={'11231'}
         >
           <Select
             value={this.state.composerText || undefined}
@@ -338,7 +356,7 @@ class App extends Component {
             onChange={val => this.onComposerInputChange(val)}
             filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
             onBlur={this.onSearch}
-            {...inputWithRemoveIconLayout}
+            {...layout}
           >
             {this.state.composers.length > 0 && this.state.composers.map(composer => <Option key={composer} value={composer}>{composer}</Option>)}
           </Select>
@@ -346,25 +364,141 @@ class App extends Component {
             {...deleteIconProps}
             onClick={() => this.emptyStateFieldAndUpdateTable('composerText')}
           />
-        </FormItem>
-        <Accordion defaultActiveKey="0" className="my-accordion">
-          {this.state.data.map(item => <Accordion.Panel 
-            header={<span><span style={ this.getDifficultyColor(_.first(item.difficulties)) }>•</span> {`${item.composer} - ${item.piece}`}</span>} className="pad">
+        </FormItem>,
+
+        <FormItem
+          {...formItemLayout}
+          label="Piece"
+          key={'11232'}
+        >
+          <Input
+            value={this.state.pieceText}
+            ref={ele => this.pieceInput = ele}
+            placeholder="Search piece"
+            onChange={(e) => this.onInputChange(e, 'pieceText')}
+            onBlur={this.onSearch}
+            {...layout}
+          />
+          <Icon
+            {...deleteIconProps}
+            onClick={() => this.emptyStateFieldAndUpdateTable('pieceText')}
+          />
+        </FormItem>,
+
+        <FormItem
+          {...formItemLayout}
+          label="Category"
+          key={'11233'}
+        >
+          <Select
+            value={this.state.categoryText || undefined}
+            showSearch
+            placeholder="Select a category"
+            optionFilterProp="children"
+            ref={ele => this.categoryInput = ele}
+            onChange={val => this.onCategoryInputChange(val)}
+            filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            onBlur={this.onSearch}
+            {...layout}
+          >
+            {this.state.categories.length > 0 && this.state.categories.map(category => <Option key={category} value={category}>{category}</Option>)}
+          </Select>
+          <Icon
+            {...deleteIconProps}
+            onClick={() => this.emptyStateFieldAndUpdateTable('categoryText')}
+          />
+        </FormItem>,
+
+        <FormItem
+          {...formItemLayout}
+          label="Publisher"
+          key={'11234'}
+        >
+          <Input
+            value={this.state.publisherText}
+            ref={ele => this.publisherInput = ele}
+            placeholder="Search publisher"
+            onChange={(e) => this.onInputChange(e, 'publisherText')}
+            onBlur={this.onSearch}
+            {...layout}
+          />
+          <Icon
+            {...deleteIconProps}
+            onClick={() => this.emptyStateFieldAndUpdateTable('publisherText')}
+          />
+        </FormItem>,
+
+        <FormItem
+          {...formItemLayout}
+          label="Difficulty"
+          key={'11235'}
+        >
+          <Slider
+            style={{ width: 200 }}
+            range
+            min={1}
+            max={13}
+            defaultValue={[1, 13]}
+            onChange={this.onDifficultySliderChange}
+            onAfterChange={this.onSearch}
+          />
+        </FormItem>,
+
+        <FormItem
+          {...tailFormItemLayout}
+          key={'11236'}
+        >
+          <Button type="primary" onClick={this.onSearch} style={{ marginRight: '20px' }}>Search</Button>
+          <Button onClick={this.onReset}>Reset</Button>
+        </FormItem>])
+    }
+
+    const footer = (extraStyles = {}) => (
+      <footer style={{ color: 'lightgrey', display: 'flex', flexDirection: 'column', fontSize: 8, ...extraStyles }}>
+        <div>Cello Repertoire Search built by <a href="https://github.com/alanbuchanan" style={{ color: 'gray' }}>Rory Smith</a></div>
+        <div>Data from <a href="http://www.cello.org/Libraries/references/syllabus.html" style={{ color: 'gray' }}>cello.org</a></div>
+        <div>
+          <a href="https://www.paypal.me/rorysmith123" style={{ color: 'gray' }}>Donate <Icon type="heart-o" style={{ color: '#bc2121' }} /></a>
+        </div>
+      </footer>
+    )
+
+    const heading = () => (
+      <h1 style={{ marginBottom: '20px' }}>Cello Repertoire Search</h1>
+    )
+
+    const mobileSectionBgColor = { backgroundColor: '#eee' }
+
+    // MOBILE
+    if (this.isDeviceMobile()) {
+      return <div>
+        <div style={{ padding: '0 10px' }}>
+          {heading()}
+        </div>
+        <div style={{ padding: '0 10px' }}>
+          {formItems(inputWithRemoveIconLayoutMobile)}
+        </div>
+
+        <Accordion className="my-accordion">
+          {this.state.data.map((item, i) => <Accordion.Panel
+            key={item.piece + item.composer + item.category + i}
+            header={<span><span style={this.getDifficultyColor(_.first(item.difficulties))}>•</span> {`${item.composer} - ${item.piece}`}</span>} className="pad">
             <List className="my-list">
-              <Item extra={`${item.publisher}`} style={{ backgroundColor: '#eee' }}>Publisher</Item>
+              <Item wrap multipleLine extra={`${item.category}`} style={mobileSectionBgColor}>Category</Item>
               <Item extra={<span style={this.getDifficultyColor(_.first(item.difficulties))}>{item.difficulties.length > 1 ? `${_.first(item.difficulties)} - ${_.last(item.difficulties)}` : _.first(item.difficulties)}</span>} style={{ backgroundColor: '#eee' }}>Difficulty</Item>
-              <Item extra={<div>
+              <Item extra={`${item.publisher}`} style={mobileSectionBgColor}>Publisher</Item>
+              <Item wrap multipleLine extra={<div>
                 <span {...linkLayout}>
                   <Tooltip placement="bottom" title="Amazon">
                     <a href={`https://www.amazon.com/s/field-keywords=${item.composer}%20${item.piece}%20cello%20sheet%20music`}>
-                      <Icon {...iconLayout} type="shopping-cart" />
+                      <Icon {...iconLayoutMobile} type="shopping-cart" />
                     </a>
                   </Tooltip>
                 </span>
                 <span {...linkLayout}>
                   <Tooltip placement="bottom" title="IMSLP">
                     <a href={`https://www.google.co.uk/search?q=imslp+${item.composer}+${item.piece}+cello`}>
-                      <Icon {...iconLayout} type="book" />
+                      <Icon {...iconLayoutMobile} type="book" />
                     </a>
                   </Tooltip>
                 </span>
@@ -372,124 +506,26 @@ class App extends Component {
                   {item.category !== 'Methods/Studies/Scale books' && (
                     <Tooltip placement="bottom" title="YouTube">
                       <a href={`https://www.youtube.com/results?search_query=${item.composer}+${item.piece}+cello`}>
-                        <Icon {...iconLayout} type="play-circle-o" />
+                        <Icon {...iconLayoutMobile} type="play-circle-o" />
                       </a>
                     </Tooltip>)}
                 </span>
-              </div>} style={{ backgroundColor: '#eee' }}>Links</Item>
+              </div>}
+                style={mobileSectionBgColor}
+              >Links</Item>
             </List>
           </Accordion.Panel>)}
         </Accordion>
 
+        {footer({ padding: '20px' })}
       </div>
     }
 
+    // DESKTOP
     return <div {...containerLayout}>
-      <h1 style={{ marginBottom: '20px' }}>Cello Repertoire Search</h1>
+      {heading()}
 
-      <FormItem
-        {...formItemLayout}
-        label="Composer"
-      >
-        <Select
-          value={this.state.composerText || undefined}
-          showSearch
-          placeholder="Search composer"
-          optionFilterProp="children"
-          ref={ele => this.composerInput = ele}
-          onChange={val => this.onComposerInputChange(val)}
-          filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-          onBlur={this.onSearch}
-          {...inputWithRemoveIconLayout}
-        >
-          {this.state.composers.length > 0 && this.state.composers.map(composer => <Option key={composer} value={composer}>{composer}</Option>)}
-        </Select>
-        <Icon
-          {...deleteIconProps}
-          onClick={() => this.emptyStateFieldAndUpdateTable('composerText')}
-        />
-      </FormItem>
-
-      <FormItem
-        {...formItemLayout}
-        label="Piece"
-      >
-        <Input
-          value={this.state.pieceText}
-          ref={ele => this.pieceInput = ele}
-          placeholder="Search piece"
-          onChange={(e) => this.onInputChange(e, 'pieceText')}
-          onBlur={this.onSearch}
-          {...inputWithRemoveIconLayout}
-        />
-        <Icon
-          {...deleteIconProps}
-          onClick={() => this.emptyStateFieldAndUpdateTable('pieceText')}
-        />
-      </FormItem>
-
-      <FormItem
-        {...formItemLayout}
-        label="Category"
-      >
-        <Select
-          value={this.state.categoryText || undefined}
-          showSearch
-          placeholder="Select a category"
-          optionFilterProp="children"
-          ref={ele => this.categoryInput = ele}
-          onChange={val => this.onCategoryInputChange(val)}
-          filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-          onBlur={this.onSearch}
-          {...inputWithRemoveIconLayout}
-        >
-          {this.state.categories.length > 0 && this.state.categories.map(category => <Option key={category} value={category}>{category}</Option>)}
-        </Select>
-        <Icon
-          {...deleteIconProps}
-          onClick={() => this.emptyStateFieldAndUpdateTable('categoryText')}
-        />
-      </FormItem>
-
-      <FormItem
-        {...formItemLayout}
-        label="Publisher"
-      >
-        <Input
-          value={this.state.publisherText}
-          ref={ele => this.publisherInput = ele}
-          placeholder="Search publisher"
-          onChange={(e) => this.onInputChange(e, 'publisherText')}
-          onBlur={this.onSearch}
-          {...inputWithRemoveIconLayout}
-        />
-        <Icon
-          {...deleteIconProps}
-          onClick={() => this.emptyStateFieldAndUpdateTable('publisherText')}
-        />
-      </FormItem>
-
-      <FormItem
-        {...formItemLayout}
-        label="Difficulty"
-      >
-        <Slider
-          style={{ width: 200 }}
-          range
-          min={1}
-          max={13}
-          defaultValue={[1, 13]}
-          onChange={this.onDifficultySliderChange}
-          onAfterChange={this.onSearch}
-        />
-      </FormItem>
-
-      <FormItem
-        {...tailFormItemLayout}
-      >
-        <Button type="primary" onClick={this.onSearch} style={{ marginRight: '20px' }}>Search</Button>
-        <Button onClick={this.onReset}>Reset</Button>
-      </FormItem>
+      {formItems(inputWithRemoveIconLayout)}
 
       <Table
         pagination={{ defaultPageSize: 25 }}
@@ -502,15 +538,7 @@ class App extends Component {
         }}
         rowKey={record => record.piece + record.composer + record.category}
       />
-
-      <footer style={{ color: 'lightgrey', display: 'flex', flexDirection: 'column', fontSize: 8 }}>
-        <div>Cello Repertoire Search built by <a href="https://github.com/alanbuchanan" style={{ color: 'gray' }}>Rory Smith</a></div>
-        <div>Data from <a href="http://www.cello.org/Libraries/references/syllabus.html" style={{ color: 'gray' }}>cello.org</a></div>
-        <div>
-          <a href="https://www.paypal.me/rorysmith123" style={{ color: 'gray' }}>Donate <Icon type="heart-o" style={{ color: '#bc2121' }} /></a>
-        </div>
-      </footer>
-
+      {footer()}
     </div>
       ;
   }
